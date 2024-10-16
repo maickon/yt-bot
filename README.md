e51cb26de991e1f790d4602e039f8e7815f21219
Maickon Rangel <maickon4developers@gmail.com>
2024-10-16 12:31:23 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
 # yt-bot
-Um bot para baixar videos do youtube e postar no seu canal
+Este é um projeto criado para o canal Maickon Rangel. Abaixo você encontra as instruções para te ajudar na configuração. O projeto tem a finalidade de conseguir postar videos no youtube através da ferramenta do google app script. O projeto usa um segundo script que é capaz de baixar os vídeos do youtube para que você possa fazer o upload no google drive.
+
+# Como obter uma chave de API do YouTube
+
+Siga os passos abaixo para gerar sua chave de API e utilizá-la no seu projeto:
+
+## Passo 1: Acesse o Google Cloud Console
+
+1. Vá para o Google Cloud Console: [https://console.cloud.google.com/](https://console.cloud.google.com/)
+2. Faça login com sua conta Google.
+
+## Passo 2: Crie um novo projeto
+
+1. No topo da página, clique no menu suspenso do seletor de projeto.
+2. Clique em "Novo Projeto".
+3. Dê um nome ao projeto (por exemplo: "API YouTube").
+4. Clique em "Criar".
+
+## Passo 3: Ative a API do YouTube
+
+1. No menu à esquerda, vá para **APIs e serviços** > **Biblioteca**.
+2. Na barra de busca, digite **YouTube Data API v3**.
+3. Clique na **YouTube Data API v3** nos resultados.
+4. Clique em "Ativar".
+
+## Passo 4: Crie credenciais de API
+
+1. Após ativar a API, vá para **APIs e serviços** > **Credenciais** no menu à esquerda.
+2. Clique em "Criar credenciais" e selecione **Chave de API**.
+3. Sua chave de API será gerada. Copie essa chave.
+
+## Passo 5: Use sua chave de API no projeto
+
+Agora, basta inserir a chave de API no seu código Python em `youtube.py`, como no exemplo abaixo:
+
+```python
+# Substitua pela sua chave de API do YouTube
+API_KEY = 'SUA_CHAVE_DE_API_AQUI'
+```
+
+# Python: Como configurar o projeto e instalar as bibliotecas necessárias
+
+Siga os passos abaixo para configurar o ambiente e instalar todas as bibliotecas necessárias para o projeto.
+
+## Passo 1: Instale o Python
+
+- Certifique-se de que você tenha o Python instalado em seu sistema.
+
+  - **Windows**: Baixe o Python a partir do site oficial: [https://www.python.org/downloads/](https://www.python.org/downloads/) e siga as instruções de instalação. Marque a opção "Add Python to PATH" durante a instalação.
+
+  - **Linux/Mac**: O Python já vem pré-instalado. Para confirmar, abra o terminal e digite:
+
+```bash
+python3 --version
+```
+
+## Passo 2: Instale o pip
+
+- O pip é o gerenciador de pacotes do Python, usado para instalar bibliotecas. Ele vem pré-instalado na maioria das instalações do Python. Verifique se o pip está instalado com:
+
+```bash
+pip --version
+```
+
+- Se não estiver instalado, siga as instruções em https://pip.pypa.io/en/stable/installation/.
+
+## Passo 3: Abra o terminal ou prompt de comando
+
+- **Windows**: Pressione `Win + R`, digite `cmd` e pressione `Enter`.
+- **Linux/Mac**: Abra o terminal.
+
+## Passo 4: Navegue até o diretório do projeto
+
+- No terminal ou prompt de comando, vá até a pasta onde o projeto está localizado. Por exemplo, se o projeto estiver na pasta `Documentos/projeto`, digite o seguinte comando:
+
+```bash
+cd Documentos/projeto
+```
+
+## Passo 5: Instale as bibliotecas necessárias
+
+- Agora que você está no diretório do projeto, execute o seguinte comando para instalar todas as bibliotecas:
+
+```bash
+pip install -r requirements.txt
+```
+
+- Este comando vai ler o arquivo requirements.txt (que contém as bibliotecas necessárias) e instalar todas elas automaticamente.
+
+
+## Passo 6: Verifique a instalação
+
+    - Depois que a instalação terminar, todas as bibliotecas mencionadas no requirements.txt estarão prontas para uso. Você pode confirmar isso tentando rodar o projeto com o Python.
+
+## Passo 7: Executar o projeto
+
+    - Agora que tudo está configurado, você pode executar o projeto normalmente usando:
+
+```bash
+    python nome_do_seu_arquivo.py
+```
---
+++ youtube.py
@@ -0,0 +1,130 @@
+import os
+import time
+import json
+import random
+from googleapiclient.discovery import build
+import yt_dlp
+
+# Substitua pela sua chave de API do YouTube
+API_KEY = 'AIzaSyAPSIIzt7C4nNC0-JpeHkXi6zF2c6aJDTc'
+
+def get_channel_id(channel_handle):
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    
+    if channel_handle.startswith('@'):
+        channel_handle = channel_handle[1:]
+
+    request = youtube.search().list(
+        q=channel_handle,
+        part='snippet',
+        type='channel',
+        maxResults=1
+    )
+    response = request.execute()
+
+    if response['items']:
+        channel_id = response['items'][0]['snippet']['channelId']
+        return channel_id
+    else:
+        raise Exception(f'Canal {channel_handle} não encontrado.')
+
+def get_videos(channel_id, max_results=10):
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    request = youtube.search().list(
+        channelId=channel_id,
+        part='id,snippet',
+        maxResults=30,
+        type='video',
+        videoDuration='medium'
+    )
+    response = request.execute()
+
+    video_data = [{'videoId': item['id']['videoId'], 'title': item['snippet']['title']} for item in response['items']]
+    return [video['videoId'] for video in video_data[:max_results]]
+    
+def get_short_videos(channel_id, max_results=10):
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    request = youtube.search().list(
+        channelId=channel_id,
+        part='id',
+        maxResults=max_results,
+        type='video',
+        videoDuration='short'
+    )
+    response = request.execute()
+
+    video_ids = [item['id']['videoId'] for item in response['items']]
+    return video_ids
+
+def download_video(video_id, download_path):
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    print(f'Tentando baixar o vídeo: {video_url}')
+    
+    ydl_opts = {
+        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
+        'format': 'mp4',
+    }
+    
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(video_url, download=True)
+            print(f'Vídeo {video_url} baixado com sucesso!')
+            return info_dict['id']  # Retorna o ID do vídeo baixado
+    except Exception as e:
+        print(f'Erro ao baixar o vídeo {video_url}: {e}')
+        return None
+
+def load_downloaded_ids(file_path):
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'r') as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            print(f"Arquivo JSON malformado: {file_path}. Retornando lista vazia.")
+            return []
+    return []
+
+def save_downloaded_ids(file_path, video_ids):
+    with open(file_path, 'w') as f:
+        json.dump(video_ids, f)
+
+def get_random_channel(channels):
+    return random.choice(channels)
+
+def main():
+    channels = [
+        "@cortesdointeligencia",
+        "@CortesPodpahOFICIAL",
+        "@cortesdoflow"
+    ]
+    
+    channel_handle = get_random_channel(channels)
+    num_videos = 20
+    download_path = 'downloads'
+    json_file_path = 'downloaded_videos.json'
+    
+    os.makedirs(download_path, exist_ok=True)
+    
+    try:
+        channel_id = get_channel_id(channel_handle) if not channel_handle.startswith('UC') else channel_handle
+        print(f'ID do Canal encontrado: {channel_id}')
+        
+        downloaded_ids = load_downloaded_ids(json_file_path)
+        video_ids = get_videos(channel_id, max_results=num_videos)
+
+        for video_id in video_ids:
+            if video_id not in downloaded_ids:
+                result = download_video(video_id, download_path)
+                if result:
+                    downloaded_ids.append(result)
+                    time.sleep(5) 
+            else:
+                print(f'O vídeo {video_id} já foi baixado. Pulando...')
+        
+        save_downloaded_ids(json_file_path, downloaded_ids)
+
+    except Exception as e:
+        print(f"Erro: {e}")
+
+if __name__ == "__main__":
+    main()
